706db0e4f32ab4d1d661cead6f387f7fb9d5a81d
Nadia <nadiawellela@gmail.com>
2023-07-24 21:19:10 -0700

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,8 +6,8 @@
  * selection_sort - sorts an array of integers in ascending order
  * using the Selection sort algorithm
  * @array: array
- * @sixe: size of the array
- * 
+ * @size: size of the array
+ *
  */
 
 void selection_sort(int *array, size_t size)
@@ -24,7 +24,7 @@ void selection_sort(int *array, size_t size)
 		for (index = i + 1; index < size; index++)
 		{
 			if (array[tmp] > array[index])
-			{	
+			{
 				tmp = index;
 				flag += 1;
 			}
