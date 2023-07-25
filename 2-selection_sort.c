da53a9dd96847762ef6b787c7ab01125cb8b3e8f
Nadia <nadiawellela@gmail.com>
2023-07-24 20:44:20 -0700

    2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * using the Selection sort algorithm
+ * @array: array
+ * @sixe: size of the array
+ * 
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, index;
+	int tmp, swap, flag;
+
+	if (!array || size == 0)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		tmp = i;
+		flag = 0;
+		for (index = i + 1; index < size; index++)
+		{
+			if (array[tmp] > array[index])
+			{	
+				tmp = index;
+				flag += 1;
+			}
+		}
+		swap = array[i];
+		array[i] = array[tmp];
+		array[tmp] = swap;
+		if (flag != 0)
+			print_array(array, size);
+
+	}
+}
